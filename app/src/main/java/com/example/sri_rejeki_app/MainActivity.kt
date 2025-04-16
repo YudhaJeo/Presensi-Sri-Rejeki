c34b8f37a75a7121c023aa0b90ce0022edfc05eb
BagasYudha <bayud.kematian@gmail.com>
2025-04-17 01:54:08 +0700

    Memperbaiki tampilan Main

--- app/src/main/java/com/example/sri_rejeki_app/MainActivity.kt
+++ app/src/main/java/com/example/sri_rejeki_app/MainActivity.kt
@@ -3,55 +3,50 @@ package com.example.sri_rejeki_app
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.sri_rejeki_app.databinding.ActivityMainBinding
 import androidx.fragment.app.FragmentTransaction
-import com.example.sri_rejeki_app.Fragment.CameraFragment
 import com.example.sri_rejeki_app.Login.LoginActivity
+import com.example.sri_rejeki_app.databinding.ActivityMainBinding
+import com.example.sri_rejeki_app.Fragment.CameraFragment
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var firebaseAuth: FirebaseAuth
 
-//    Data karyawan dari Login
-    val sharedPref = getSharedPreferences("user_session", MODE_PRIVATE)
-    val username = sharedPref.getString("username", "")
-    val email = sharedPref.getString("email", "")
-    val fullName = sharedPref.getString("fullName", "")
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Menginisialisasi binding
         binding = ActivityMainBinding.inflate(layoutInflater)
-
-        // Set layout menggunakan root view dari binding
         setContentView(binding.root)
 
         firebaseAuth = FirebaseAuth.getInstance()
 
-        // Menambahkan listener pada ivButtonScan
+        val sharedPref = getSharedPreferences("user_session", MODE_PRIVATE)
+
+        // Set nama user
+        val username = sharedPref.getString("username", "User")
+        binding.tvHalo.text = "Hai, $username!"
+
+        // Kamera Fragment
         binding.ivButtonScan.setOnClickListener {
-            // Menampilkan CameraFragment saat button di klik
             val fragment = CameraFragment()
             val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
-            transaction.replace(R.id.camera_fragment, fragment) // Pastikan ada container untuk menampung fragment
+            transaction.replace(R.id.camera_fragment, fragment)
             transaction.addToBackStack(null)
             transaction.commit()
         }
 
+        // Logout
         binding.btnLogout.setOnClickListener {
-            firebaseAuth.signOut() // hapus sesi login dari Firebase
-            // Hapus sesi login di SharedPreferences
-            val sharedPref = getSharedPreferences("user_session", MODE_PRIVATE)
-            sharedPref.edit().clear().apply() // menghapus data session yang tersimpan
+            firebaseAuth.signOut()
+            sharedPref.edit().clear().apply()
 
-            // Pindah ke LoginActivity setelah logout
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
             finish()
         }
     }
 }
+
